b49256a1c1faabea40ce61884e4eb562b89519fe
imdante <763093576@qq.com>
2018-11-07 15:15:32 +0800

    添加七牛上传sdk

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,6 +7,7 @@ namespace Composer\Autoload;
 class ComposerStaticInit650d1782257cfad0cabd0f8b2c1a4775
 {
     public static $files = array (
+        '841780ea2e1d6545ea3a253239d59c05' => __DIR__ . '/..' . '/qiniu/php-sdk/src/Qiniu/functions.php',
         '1cfd2761b63b0a29ed23657ea394cb2d' => __DIR__ . '/..' . '/topthink/think-captcha/src/helper.php',
     );
 
@@ -19,6 +20,10 @@ class ComposerStaticInit650d1782257cfad0cabd0f8b2c1a4775
         'a' => 
         array (
             'app\\' => 4,
+        ),
+        'Q' => 
+        array (
+            'Qiniu\\' => 6,
         ),
     );
 
@@ -34,6 +39,10 @@ class ComposerStaticInit650d1782257cfad0cabd0f8b2c1a4775
         'app\\' => 
         array (
             0 => __DIR__ . '/../..' . '/application',
+        ),
+        'Qiniu\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/qiniu/php-sdk/src/Qiniu',
         ),
     );
 
